4e62e225435c09c9be2c4e22fd9245d1fe0b8f51
94ju <janithherath94@gmail.com>
2018-09-12 17:09:09 +0530

    Adding node modules

--- js/app.js
+++ js/app.js
@@ -4,3 +4,5 @@ const nameContainer = document.querySelector(".main__profile-name");
 const unContainer = document.querySelector(".main__profile-username");
 const reposContainer = document.querySelector(".main__profile-repos");
 const urlContainer = document.querySelector(".main__profile-url");
+
+ searchButton.addEventListner("click",()=>{console.log("Hello");})
