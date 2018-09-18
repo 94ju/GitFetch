2c04ced86d5df0690285fd92042d803c47fa1a45
94ju <janithherath94@gmail.com>
2018-09-18 16:37:28 +0530

    Commit details

--- js/app.js
+++ js/app.js
@@ -14,9 +14,10 @@ const client_secret = "051782808038d3d46977a6ebb24fe2b5a4f0620c";
 const fectchUsers = async(user) => {
 const api_call =await fetch('https://api.github.com/users/'+user+'?client_id='+client_id+'&client_secret='+client_secret);
 const api_call_repos =await fetch(' https://api.github.com/users/'+user+'/repos');
-  const data = await api_call.json();
-  const data_repos = await api_call_repos.json();
-  return{data};
+
+const data = await api_call.json();
+const data_repos = await api_call_repos.json();
+return{data};
 };
 const fectchrepos = async(user) => {
 
@@ -24,6 +25,12 @@ const api_call_repos =await fetch(' https://api.github.com/users/'+user+'/repos'
 const data_repos = await api_call_repos.json();
 return{data_repos};
 };
+const fectcommits = async(user) => {
+
+const api_call_commits = await fetch(' https://api.github.com/repos/'+user+'/GitFetch/commits');
+const data_repos = await api_call_commits.json();
+return{data_repos};
+}
 
 const showData =() => {
   fectchUsers(inputValue.value).then((res)=>{
@@ -38,15 +45,16 @@ const showData =() => {
   fectchrepos(inputValue.value).then((rep)=>{
     var i=0;
     console.log(rep);
-
-
       for(i;i<14;i++){
         console.log(rep.data_repos[i].name);
-
       }
 
-    console.log("check");
+  })
+  fectcommits(inputValue.value).then((com)=>{
 
+    console.log(com);
+
+    console.log("check");
   })
 };
 
